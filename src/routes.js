906937806c6da4f4284a1271dfe3f0b329075235
Alex Sousa <alexsousa1435@gmail.com>
2020-09-14 22:05:52 -0300

    Adicionando Users para autenticacao

--- src/routes.js
+++ src/routes.js
@@ -3,21 +3,23 @@ const express = require('express');
 const routes = express.Router();
 
 const ProductController = require('./controllers/ProductController');
+const UserController = require('./controllers/UserController');
 
-routes.get('/', (req, res) => {
-    /*Product.create({
-        title: 'Alex Sousa',
-        description: 'Lindao',
-        url: 'www.google.com'
-    });*/
-
-    return res.send('Nova rota!');
-});
+/* routes.get('/', (req, res) => {
+    res.send('Ola mundo!');
+}); */
 
 routes.get('/products', ProductController.index);
+routes.post('/products', ProductController.store);
 routes.get('/products/:id', ProductController.show);
 routes.put('/products/:id', ProductController.update);
 routes.delete('/products/:id', ProductController.destroy);
-routes.post('/products', ProductController.store)
+
+routes.get('/users', UserController.index);
+routes.get('/users/:id', UserController.show);
+routes.post('/users', UserController.store);
+routes.put('/users/:id', UserController.update);
+routes.delete('/users/:id', UserController.destroy);
+
 
 module.exports = routes;
